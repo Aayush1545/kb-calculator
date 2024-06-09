013e3cc30b7b3c9cc2ce36b8de42526c26f262c4
Aayush Rauniyar <140833225+Aayush1545@users.noreply.github.com>
2024-06-09 19:10:40 +0545

    Update script.js

--- script.js
+++ script.js
@@ -551,6 +551,376 @@ function handleAutoFill(event) {
             else if (value === '3.211' && thirdColInput) {
                 thirdColInput.value = '1.72';
             }
+
+
+            //d-grade
+
+             else if (value === '4.30d' && fifthColInput) {
+                fifthColInput.value = '2.39';
+            }
+            else if (value === '4.31d' && fifthColInput) {
+                fifthColInput.value = '2.52';
+            }
+            else if (value === '4.32d' && fifthColInput) {
+                fifthColInput.value = '2.66';
+            }
+            else if (value === '4.33d' && fifthColInput) {
+                fifthColInput.value = '2.80';
+            }
+            else if (value === '4.34d' && fifthColInput) {
+                fifthColInput.value = '2.95';
+            }
+            else if (value === '4.35d' && fifthColInput) {
+                fifthColInput.value = '3.10';
+            }
+            else if (value === '4.36d' && fifthColInput) {
+                fifthColInput.value = '3.25';
+            }
+            else if (value === '4.37d' && fifthColInput) {
+                fifthColInput.value = '3.41';
+            }
+            else if (value === '4.38d' && fifthColInput) {
+                fifthColInput.value = '3.57';
+            }
+            else if (value === '4.39d' && fifthColInput) {
+                fifthColInput.value = '3.73';
+            }
+            else if (value === '4.310d' && fifthColInput) {
+                fifthColInput.value = '3.90';
+            }
+            else if (value === '4.311d' && fifthColInput) {
+                fifthColInput.value = '4.07';
+            }
+            else if (value === '4.40d' && fifthColInput) {
+                fifthColInput.value = '4.25';
+            }
+            else if (value === '4.41d' && fifthColInput) {
+                fifthColInput.value = '4.42';
+            }
+            else if (value === '4.42d' && fifthColInput) {
+                fifthColInput.value = '4.61';
+            }
+            else if (value === '4.43d' && fifthColInput) {
+                fifthColInput.value = '4.79';
+            }
+            else if (value === '4.44d' && fifthColInput) {
+                fifthColInput.value = '4.98';
+            }
+            else if (value === '4.45d' && fifthColInput) {
+                fifthColInput.value = '5.18';
+            }
+            else if (value === '4.46d' && fifthColInput) {
+                fifthColInput.value = '5.37';
+            }
+            else if (value === '4.47d' && fifthColInput) {
+                fifthColInput.value = '5.57';
+            }
+            else if (value === '4.48d' && fifthColInput) {
+                fifthColInput.value = '5.78';
+            }
+            else if (value === '4.49d' && fifthColInput) {
+                fifthColInput.value = '5.99';
+            }
+            else if (value === '4.410d' && fifthColInput) {
+                fifthColInput.value = '6.20';
+            }
+            else if (value === '4.411d' && fifthColInput) {
+                fifthColInput.value = '6.42';
+            }
+            else if (value === '4.50d' && fifthColInput) {
+                fifthColInput.value = '6.64';
+            }
+            else if (value === '4.51d' && fifthColInput) {
+                fifthColInput.value = '6.86';
+            }
+            else if (value === '4.52d' && fifthColInput) {
+                fifthColInput.value = '7.09';
+            }
+            else if (value === '4.53d' && fifthColInput) {
+                fifthColInput.value = '7.32';
+            }
+            else if (value === '4.54d' && fifthColInput) {
+                fifthColInput.value = '7.55';
+            }
+            else if (value === '4.55d' && fifthColInput) {
+                fifthColInput.value = '7.79';
+            }
+            else if (value === '4.56d' && fifthColInput) {
+                fifthColInput.value = '8.03';
+            }
+            else if (value === '4.57d' && fifthColInput) {
+                fifthColInput.value = '8.28';
+            }
+            else if (value === '4.58d' && fifthColInput) {
+                fifthColInput.value = '8.52';
+            }
+            else if (value === '4.59d' && fifthColInput) {
+                fifthColInput.value = '8.78';
+            }
+            else if (value === '4.510d' && fifthColInput) {
+                fifthColInput.value = '9.03';
+            }
+            else if (value === '4.511d' && fifthColInput) {
+                fifthColInput.value = '9.20';
+            }
+            else if (value === '4.60d' && fifthColInput) {
+                fifthColInput.value = '9.56';
+            }
+            else if (value === '4.61d' && fifthColInput) {
+                fifthColInput.value = '9.82';
+            }
+            else if (value === '4.62d' && fifthColInput) {
+                fifthColInput.value = '10.10';
+            }
+            else if (value === '4.63d' && fifthColInput) {
+                fifthColInput.value = '10.37';
+            }
+            else if (value === '4.64d' && fifthColInput) {
+                fifthColInput.value = '10.65';
+            }
+            else if (value === '4.65d' && fifthColInput) {
+                fifthColInput.value = '10.93';
+            }
+            else if (value === '4.66d' && fifthColInput) {
+                fifthColInput.value = '11.22';
+            }
+            else if (value === '4.67d' && fifthColInput) {
+                fifthColInput.value = '11.51';
+            }
+            else if (value === '4.68d' && fifthColInput) {
+                fifthColInput.value = '11.80';
+            }
+            else if (value === '4.69d' && fifthColInput) {
+                fifthColInput.value = '12.10';
+            }
+            else if (value === '4.610d' && fifthColInput) {
+                fifthColInput.value = '12.40';
+            }
+            else if (value === '4.611d' && fifthColInput) {
+                fifthColInput.value = '12.70';
+            }
+            else if (value === '4.70d' && fifthColInput) {
+                fifthColInput.value = '13.01';
+            }
+            else if (value === '4.20d' && fifthColInput) {
+                fifthColInput.value = '1.06';
+            }
+            else if (value === '4.21d' && fifthColInput) {
+                fifthColInput.value = '1.15';
+            }
+            else if (value === '4.22d' && fifthColInput) {
+                fifthColInput.value = '1.24';
+            }
+            else if (value === '4.23d' && fifthColInput) {
+                fifthColInput.value = '1.34';
+            }
+            else if (value === '4.24d' && fifthColInput) {
+                fifthColInput.value = '1.44';
+            }
+            else if (value === '4.25d' && fifthColInput) {
+                fifthColInput.value = '1.55';
+            }
+            else if (value === '4.26d' && fifthColInput) {
+                fifthColInput.value = '1.66';
+            }
+            else if (value === '4.27d' && fifthColInput) {
+                fifthColInput.value = '1.77';
+            }
+            else if (value === '4.28d' && fifthColInput) {
+                fifthColInput.value = '1.88';
+            }
+            else if (value === '4.29d' && fifthColInput) {
+                fifthColInput.value = '2.00';
+            }
+            else if (value === '4.210d' && fifthColInput) {
+                fifthColInput.value = '2.13';
+            }
+            else if (value === '4.211d' && fifthColInput) {
+                fifthColInput.value = '2.25';
+            }
+            else if (value === '3.30d' && fifthColInput) {
+                fifthColInput.value = '1.82';
+            }
+            else if (value === '3.31d' && fifthColInput) {
+                fifthColInput.value = '1.93';
+            }
+            else if (value === '3.32d' && fifthColInput) {
+                fifthColInput.value = '2.03';
+            }
+            else if (value === '3.33d' && fifthColInput) {
+                fifthColInput.value = '2.14';
+            }
+            else if (value === '3.34d' && fifthColInput) {
+                fifthColInput.value = '2.25';
+            }
+            else if (value === '3.35d' && fifthColInput) {
+                fifthColInput.value = '2.37';
+            }
+            else if (value === '3.36d' && fifthColInput) {
+                fifthColInput.value = '2.48';
+            }
+            else if (value === '3.37d' && fifthColInput) {
+                fifthColInput.value = '2.60';
+            }
+            else if (value === '3.38d' && fifthColInput) {
+                fifthColInput.value = '2.73';
+            }
+            else if (value === '3.39d' && fifthColInput) {
+                fifthColInput.value = '2.85';
+            }
+            else if (value === '3.310d' && fifthColInput) {
+                fifthColInput.value = '2.98';
+            }
+            else if (value === '3.311d' && fifthColInput) {
+                fifthColInput.value = '3.11';
+            }
+            else if (value === '3.40d' && fifthColInput) {
+                fifthColInput.value = '3.25';
+            }
+            else if (value === '3.41d' && fifthColInput) {
+                fifthColInput.value = '3.38';
+            }
+            else if (value === '3.42d' && fifthColInput) {
+                fifthColInput.value = '3.52';
+            }
+            else if (value === '3.43d' && fifthColInput) {
+                fifthColInput.value = '3.66';
+            }
+            else if (value === '3.44d' && fifthColInput) {
+                fifthColInput.value = '3.81';
+            }
+            else if (value === '3.45d' && fifthColInput) {
+                fifthColInput.value = '3.96';
+            }
+            else if (value === '3.46d' && fifthColInput) {
+                fifthColInput.value = '4.11';
+            }
+            else if (value === '3.47d' && fifthColInput) {
+                fifthColInput.value = '4.26';
+            }
+            else if (value === '3.48d' && fifthColInput) {
+                fifthColInput.value = '4.42';
+            }
+            else if (value === '3.49d' && fifthColInput) {
+                fifthColInput.value = '4.58';
+            }
+            else if (value === '3.410d' && fifthColInput) {
+                fifthColInput.value = '4.74';
+            }
+            else if (value === '3.411d' && fifthColInput) {
+                fifthColInput.value = '4.91';
+            }
+            else if (value === '3.50d' && fifthColInput) {
+                fifthColInput.value = '5.07';
+            }
+            else if (value === '3.51d' && fifthColInput) {
+                fifthColInput.value = '5.24';
+            }
+            else if (value === '3.52d' && fifthColInput) {
+                fifthColInput.value = '5.42';
+            }
+            else if (value === '3.53d' && fifthColInput) {
+                fifthColInput.value = '5.59';
+            }
+            else if (value === '3.54d' && fifthColInput) {
+                fifthColInput.value = '5.77';
+            }
+            else if (value === '3.55d' && fifthColInput) {
+                fifthColInput.value = '5.95';
+            }
+            else if (value === '3.56d' && fifthColInput) {
+                fifthColInput.value = '6.14';
+            }
+            else if (value === '3.57d' && fifthColInput) {
+                fifthColInput.value = '6.33';
+            }
+            else if (value === '3.58d' && fifthColInput) {
+                fifthColInput.value = '6.52';
+            }
+            else if (value === '3.59d' && fifthColInput) {
+                fifthColInput.value = '6.71';
+            }
+            else if (value === '3.510d' && fifthColInput) {
+                fifthColInput.value = '6.91';
+            }
+            else if (value === '3.511d' && fifthColInput) {
+                fifthColInput.value = '7.11';
+            }
+            else if (value === '3.60d' && fifthColInput) {
+                fifthColInput.value = '7.31';
+            }
+            else if (value === '3.61d' && fifthColInput) {
+                fifthColInput.value = '7.51';
+            }
+            else if (value === '3.62d' && fifthColInput) {
+                fifthColInput.value = '7.72';
+            }
+            else if (value === '3.63d' && fifthColInput) {
+                fifthColInput.value = '7.93';
+            }
+            else if (value === '3.64d' && fifthColInput) {
+                fifthColInput.value = '8.14';
+            }
+            else if (value === '3.65d' && fifthColInput) {
+                fifthColInput.value = '8.36';
+            }
+            else if (value === '3.66d' && fifthColInput) {
+                fifthColInput.value = '8.58';
+            }
+            else if (value === '3.67d' && fifthColInput) {
+                fifthColInput.value = '8.80';
+            }
+            else if (value === '3.68d' && fifthColInput) {
+                fifthColInput.value = '9.02';
+            }
+            else if (value === '3.69d' && fifthColInput) {
+                fifthColInput.value = '9.25';
+            }
+            else if (value === '3.610d' && fifthColInput) {
+                fifthColInput.value = '9.48';
+            }
+            else if (value === '3.611d' && fifthColInput) {
+                fifthColInput.value = '9.71';
+            }
+            else if (value === '3.70d' && fifthColInput) {
+                fifthColInput.value = '9.95';
+            }
+            else if (value === '3.20d' && fifthColInput) {
+                fifthColInput.value = '0.81';
+            }
+            else if (value === '3.21d' && fifthColInput) {
+                fifthColInput.value = '0.88';
+            }
+            else if (value === '3.22d' && fifthColInput) {
+                fifthColInput.value = '0.95';
+            }
+            else if (value === '3.23d' && fifthColInput) {
+                fifthColInput.value = '1.02';
+            }
+            else if (value === '3.24d' && fifthColInput) {
+                fifthColInput.value = '1.10';
+            }
+            else if (value === '3.25d' && fifthColInput) {
+                fifthColInput.value = '1.18';
+            }
+            else if (value === '3.26d' && fifthColInput) {
+                fifthColInput.value = '1.26';
+            }
+            else if (value === '3.27d' && fifthColInput) {
+                fifthColInput.value = '1.35';
+            }
+            else if (value === '3.28d' && fifthColInput) {
+                fifthColInput.value = '1.44';
+            }
+            else if (value === '3.29d' && fifthColInput) {
+                fifthColInput.value = '1.53';
+            }
+            else if (value === '3.210d' && fifthColInput) {
+                fifthColInput.value = '1.63';
+            }
+            else if (value === '3.211d' && fifthColInput) {
+                fifthColInput.value = '1.72';
+            }    
             else {
                 secondColInput.value = '';
                 thirdColInput.value = '';
